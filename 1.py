9f52fe6c8e6a32148ed276a5119a39beb1543919
“jjgm666git config --global user.email “mingjun9522@hotmail.com <jjgm666>
2018-01-13 16:12:24 +0800

    基本程序设计

--- 1.py
+++ 1.py
@@ -5,3 +5,25 @@ print("{0} was {1} years old".format(name,age))
 
 #字符串的连接   +
 print(name + " was " +str(age) + " years old")
+
+#程序自带数据
+width = 5.5
+height = 2
+print("area is",width * height)
+
+
+#从控制台读取输入(输入的是一个字符串，可以用eval函数将其转换为  数值型！！
+radius = eval(input("Enter a value for radius:"))
+area = radius * radius * 3.14159
+print("The area for the circle of radius",radius,"is:",area)
+
+#交换两个参数的值
+x = 1
+y = 2
+x , y = y , x
+print(x,y)
+
+#多值输入
+number1 , number2 , number3 = eval(input("Enter three numbers separated by commas:"))
+average = (number1 + number2 + number3) /3
+print("The average of", number1 , number2 , number3 , "is" , average)
