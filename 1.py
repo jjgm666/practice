3d355154e8f9d5d0df906e5a151bfd24501e87e2
“jjgm666git config --global user.email “mingjun9522@hotmail.com <jjgm666>
2018-01-16 11:15:58 +0800

    基本程序设计

--- 1.py
+++ 1.py
@@ -12,7 +12,7 @@ height = 2
 print("area is",width * height)
 
 
-#从控制台读取输入(输入的是一个字符串，可以用eval函数将其转换为  数值型！！
+#从控制台读取输入(输入的是一个字符串，    可以用  eval()   函数   将其转换为  数值型！！
 radius = eval(input("Enter a value for radius:"))
 area = radius * radius * 3.14159
 print("The area for the circle of radius",radius,"is:",area)
@@ -27,3 +27,46 @@ print(x,y)
 number1 , number2 , number3 = eval(input("Enter three numbers separated by commas:"))
 average = (number1 + number2 + number3) /3
 print("The average of", number1 , number2 , number3 , "is" , average)
+
+#运算符——   +  -  /  //  **   %
+a = 1 + 2
+b = 1 - 2
+c = 1 * 2
+d = 1 / 2
+e = 1 // 2
+f = 4 ** 2
+g = 20 % 3
+
+print("加法a=",a,",减法b=",b,"乘法c=",c,",除法d=",d,",取整e=",e,",求幂f=",f,",取余(取模)g=",g)
+
+#以秒计时的一段时间转换为以分和秒计时的时间
+seconds = eval(input("输入一个以秒计时的一段时间："))
+minutes = seconds // 60
+newseconds = seconds % 60
+print(seconds,"转换成",minutes,"分",newseconds,"秒")
+
+#科学记数法
+#1.23456E2   123.456E-2
+
+#增强运算符
+'''
+a+=4    a=a+4
+a-=4    a=a-4
+a*=4    a=a*4
+a/=4    a=a/4
+a//=4   a=a//4
+a%=4    a=a%4
+a**=4   a=a**4
+'''
+
+#类型转换   四舍五入
+value = 5.6
+a = int(value)
+b = round(value)
+c = round(5.2)
+print(a,b,c)
+
+#营业税保留两位小数
+purchaseAmount=eval(input("输入营业额："))
+tax=purchaseAmount*0.06
+print("营业额为",int(tax*100)/100.0)  #保留了两位小数
