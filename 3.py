03c54083503e097c9c591314e038cb699f20c43f
“jjgm666git config --global user.email “mingjun9522@hotmail.com <jjgm666>
2018-01-22 10:58:21 +0800

    if选择，逻辑用法

--- 3.py
+++ 3.py
@@ -7,10 +7,53 @@ print(radius < 0)
 print(int(radius > 0))
 print(int(radius < 0))
 
-# 产生随机数  random()函数,需要引入random包,用法是：random.randint(a , b)
+# 产生随机数  random()函数,需要引入random包,用法是：random.randint(a , b)  返回的是从a到b的随机整数；
+#                                        也可以用random.randrange(a , b)  返回的是从a到b-1的随机整数；
+#                                        还可以用random.random()     返回的是从0.0到1.0的随机浮点数；
 import random
 
-number1 = random.randint(0,20)
-number2 = random.randint(0,20)
-answer = eval(input("What is " + str(number1) + "+" +str(number2) + "?"))
-print(number1,"+",number2,"=",answer,"is",number1+number2==answer)
+number1 = random.randint(0, 20)
+number2 = random.randint(0, 20)
+answer = eval(input("What is " + str(number1) + "+" + str(number2) + "?"))
+print(number1, "+", number2, "=", answer, "is", number1 + number2 == answer)
+
+# if语句
+score = eval(input("输入成绩："))
+if score >= 90:
+    grade = 'A'
+else:
+    if score >= 80:
+        grade = "B"
+    else:
+        if score >= 70:
+            grade = 'C'
+        else:
+            if score >= 60:
+                grade = 'D'
+            else:
+                grade = 'E'
+print("Grade is :", grade)
+
+score1 = eval(input("输入成绩1："))
+if score1 >= 90:
+    grade = 'A'
+elif score >= 80:
+        grade = "B"
+elif score >= 70:
+        grade = 'C'
+elif score >= 60:
+        grade = 'D'
+else:
+        grade = 'E'
+print("Grade is :", grade)
+
+# 逻辑运算符 not 逻辑否、and 逻辑和、or 逻辑或
+number = eval(input("输入一个整数:"))
+
+if number % 2 == 0 and number % 3 == 0:
+    print("这个数能被2和3整除！")
+elif number % 2 == 0 or number % 3 == 0:
+    print("这个数能被2或3整除！")
+elif (number % 2 == 0 or number % 3 == 0) and not (number % 2 == 0 and number % 3 == 0):
+    print("这个数能被2或3整除,但不能被2或3整除！")
+
